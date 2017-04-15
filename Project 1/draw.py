e5c6afbe51ccb0d664d2a8f69577edd518a6cc89
vinacovre <viniassis@me.com>
2017-04-15 14:17:17 -0300

    separating constants and considering padding for further average filter application

--- Project 1/draw.py
+++ Project 1/draw.py
@@ -7,6 +7,8 @@ Group: Vinicius Covre, Bianca Privati, Felipe Pipoca
 # Import a library of functions
 import pygame, sys
 from pygame.locals import *
+import constants
+from constants import SIZE, WHITE
 import bresenham_line
 import bresenham_circle
 
@@ -20,17 +22,18 @@ print("\n********************************")
 if str.lower(answer) == 'a':
     print("******** BRESENHAM LINE ********")
     print("\nFrom point:\n")
-    x0 = int(input("\tx0: "))
-    y0 = int(input("\ty0: "))
+    # +1 to place the pixel considering the padding
+    x0 = int(input("\tx0: ")) + 1
+    y0 = int(input("\ty0: ")) + 1
     print("\nTo point:\n")
-    xF = int(input("\txF: "))
-    yF = int(input("\tyF: "))
+    xF = int(input("\txF: ")) + 1
+    yF = int(input("\tyF: ")) + 1
     listOfPoints = bresenham_line.getLine((x0,y0),(xF,yF))
 else:
     print("******* BRESENHAM CIRCLE *******")
     print("\nCenter:\n")
-    xCenter = int(input("\txCenter: "))
-    yCenter = int(input("\tyCenter: "))
+    xCenter = int(input("\txCenter: ")) + 1
+    yCenter = int(input("\tyCenter: ")) + 1
     radius = int(input("\tRadius: "))
     listOfPoints = bresenham_circle.getCircle((xCenter,yCenter),radius)
 
@@ -40,8 +43,7 @@ print("\n********************************\n")
 pygame.init()
 
 # Set (width X height) window
-size = (500, 500)
-windowSurface = pygame.display.set_mode(size, 0, 32)
+windowSurface = pygame.display.set_mode(SIZE, 0, 32)
 
 # Set caption in the display
 pygame.display.set_caption('Bresenham')
@@ -50,7 +52,6 @@ pygame.display.set_caption('Bresenham')
 pixArray = pygame.PixelArray(windowSurface)
 
 # Print obtained pixels
-WHITE = (255, 255, 255)
 for i in range(len(listOfPoints)):
     x = listOfPoints[i][0]
     y = listOfPoints[i][1]
