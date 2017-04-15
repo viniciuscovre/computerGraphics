7b93b655d513fbee3d2edbe053157f7a2a674eed
vinacovre <viniassis@me.com>
2017-04-15 14:16:02 -0300

    separating program constants

---
+++ Project 1/constants.py
@@ -0,0 +1,14 @@
+# Pygame Window width and height
+WIDTH = 500
+HEIGHT = 500
+
+# +1 is to consider the actual border value, since it goes from 0 to VALUE-1
+WIDTH += 1
+HEIGHT += 1
+
+# Pygame Window size
+SIZE = (WIDTH + 2, HEIGHT + 2) # "+2" is the padding ... +2 rows and +2 cols
+# From 0 to WIDTH-1 and from 0 to HEIGHT-1
+
+# Pixel Color
+WHITE = (255, 255, 255)
