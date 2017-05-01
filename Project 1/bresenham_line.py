4d8500a24b00fdda38c4017241496be168157b6c
vinacovre <viniassis@me.com>
2017-05-01 15:50:21 -0300

    improving performance

--- Project 1/bresenham_line.py
+++ Project 1/bresenham_line.py
@@ -3,47 +3,48 @@ def getLine(start, end):
     Bresenham's Line Algorithm
     Produces a list of tuples/pixels (x,y) to be printed
     """
-    # Setup initial conditions
+    # Setting up initial values
+    points = []
     x1, y1 = start
     x2, y2 = end
-    dx = x2 - x1
-    dy = y2 - y1
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
 
-    # Determine how steep the line is
-    is_steep = abs(dy) > abs(dx)
-
-    # Rotate line
-    if is_steep:
+    # Checking if line is close to y-axis
+    invertedCoordinates = False
+    if dy > dx:
         x1, y1 = y1, x1
         x2, y2 = y2, x2
+        invertedCoordinates = True
 
-    # Swap start and end points if necessary and store swap state
-    swapped = False
+    # Get the initial point as the closest to the center (0,0)
+    invertedPoints = False
     if x1 > x2:
         x1, x2 = x2, x1
         y1, y2 = y2, y1
-        swapped = True
+        invertedPoints = True
 
-    # Recalculate differentials
+    # Recalculating differentials
     dx = x2 - x1
     dy = y2 - y1
 
-    # Calculate error
-    error = int(dx / 2.0)
-    ystep = 1 if y1 < y2 else -1
+    # Calculating d value
+    d = int(dx / 2.0)
 
-    # Iterate over bounding box generating points between start and end
+    # Calculating increment in y
+    yInc = 1 if y1 < y2 else -1
+
+    # Iterate to generate points of the line
     y = y1
-    points = []
     for x in range(x1, x2 + 1):
-        coord = (y, x) if is_steep else (x, y)
-        points.append(coord)
-        error -= abs(dy)
-        if error < 0:
-            y += ystep
-            error += dx
+        pixel = (y, x) if invertedCoordinates else (x,y)
+        points.append(pixel)
+        d -= abs(dy)
+        if d < 0:
+            y += yInc
+            d += dx
 
-    # Reverse the list if the coordinates were swapped
-    if swapped:
+    # If the start and end points are inverted, reverse the list
+    if invertedPoints:
         points.reverse()
     return points
