5a1f9b79aa9aed5b2870b14d1274fbc97431af51
vinacovre <viniassis@me.com>
2017-05-24 12:37:41 -0300

    inserting blind deconvolution option

--- Deblurring/deblurringMenu.m
+++ Deblurring/deblurringMenu.m
@@ -3,8 +3,10 @@ function deblurredImage = deblurringMenu(blurredImage)
 
 fprintf('\n--> DEBLURRING STEP');
 fprintf('\nSelect the deblurring methodology:\n');
-fprintf('\n  1 - Lucy-Richardson Algorithm');
-fprintf('\n  2 - Wiener Filter');
+fprintf('\n  1 - Lucy-Richardson Algorithm'); % for Gaussian Blur
+fprintf('\n  2 - Wiener Deconvolution'); % for Wiener Blur (good for Motion Blur)
+fprintf('\n  3 - Motion Deblur (using Wiener Deconvolution)'); % for Motion Blur
+fprintf('\n  4 - Blind Deconvolution'); % test for all cases
 answer = input('\n\nAnswer: ');
 
 switch answer
@@ -14,6 +16,12 @@ switch answer
     case 2
         % Wiener Filter
         deblurredImage = wienerDeblur(blurredImage);
+    case 3
+        % Wiener Filter
+        deblurredImage = motionDeblur(blurredImage);
+    case 4
+        % Blind Deblurring
+        deblurredImage = blindDeblur(blurredImage);
     otherwise
         fprintf('Invalid option! Choose a valid one...\n');
         deblurredImage = deblurringMenu(blurredImage);
